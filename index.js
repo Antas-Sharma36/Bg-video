0e2fecdfc4e52abaafebdf4fcaea2035f831fda9
Antas-Sharma36 <sharmaantas20@gmail.com>
2023-02-06 22:01:06 +0530

    Check working?

--- index.js
+++ index.js
@@ -3,16 +3,28 @@ const video = document.querySelector(".bg_video");
 const fa = document.querySelector(".fa-solid");
 
 btn.addEventListener("click",()=>{
-    if ( btn.classList.contains("pause")){
+    console.log(btn.classList.values);
+    if ( btn.classList.contains("pause") ){
         btn.classList.remove("pause");
         video.play();
         fa.classList.add("fa-pause");
         fa.classList.remove("fa-play");
-    } else {
+        console.log("Playing");
+    } else if (  !(btn.classList.contains("pause") || btn.classList.contains("play")) ){
+        video.play();
+        console.log(btn.classList.value);
+        fa.classList.remove("play");
+        
+        fa.classList.add("pause");
+        console.log(btn.classList.value);
+        
+        
+    } 
+    else {
         btn.classList.add("pause");
         video.pause();
         fa.classList.remove("fa-pause");
         fa.classList.add("fa-play");
-        
-    }
+        console.log("Pausing");
+    } 
 })
